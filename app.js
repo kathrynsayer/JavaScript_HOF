fd4b31a1df7af9a11f24d6e911b64e214a967d78
kathrynsayer <kssayer@gmail.com>
2022-08-19 13:19:30 -0700

    first 3 exercises

--- app.js
+++ app.js
@@ -2,3 +2,63 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+function plus(num1) {
+    return function (plusNumber) {
+        return plusNumber + num1
+
+    }
+}
+let plus15 = plus(15)
+
+console.log(plus15(10));
+
+// Exercise 2 Section
+console.log("EXERCISE 2:\n==========\n");
+
+let users = [
+    {
+        name: "Frodo",
+        age: 50,
+        score: 85,
+        isActive: false,
+    },
+    {
+        name: "Sam",
+        age: 38,
+        score: 94,
+        isActive: true,
+    },
+    {
+        name: "Merry",
+        age: 36,
+        score: 82,
+        isActive: true,
+    },
+    {
+        name: "Pippin",
+        age: 26,
+        score: 77,
+        isActive: false,
+    },
+];
+
+users.forEach((item) => {
+    console.log(item.name, item.age, item.score, item.isActive);
+}
+)
+
+// Exercise 3 Section
+console.log("EXERCISE 3:\n==========\n");
+
+let nameScore = users.map((item) => {
+    return {
+        name: item.name, score: item.score
+    };
+})
+
+console.log(nameScore); 
+
+// Exercise 4 Section
+console.log("EXERCISE 4:\n==========\n");
+
